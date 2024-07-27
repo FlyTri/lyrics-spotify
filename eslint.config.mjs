b1e3a77f01ad990a3658e2fe06b95339b4cc61d2
Võ Minh Trí <vominhtri28032010@gmail.com>
2024-07-27 17:30:10 +0700

    yarn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  {
-    ignores: ["public/scripts/*", "public/data/*", "dist/*", "node_modules/*"],
-  },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
